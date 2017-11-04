42816f1f6011f62c97fdeabe828640a27eada0cd
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2017-11-04 09:33:59 -0500

    ya funcina el cacharro
    al fin saca el letreto

--- EnLinea/src/presentacion/EnLineaGUI.java
+++ EnLinea/src/presentacion/EnLineaGUI.java
@@ -1,8 +1,10 @@
 package presentacion;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.*;
 
 public class EnLineaGUI extends JFrame{
 	
@@ -10,16 +12,14 @@ public class EnLineaGUI extends JFrame{
 	private EnLineaGUI(){
 		prepareElementos();
 		prepareAcciones();
-		setDefaultCloseOperation(3);
-
-		//inicie();
 	}
 
 	public static void main(String[] args){
 		EnLineaGUI ventana = new EnLineaGUI();
 		ventana.setVisible(true);
+		ventana.addWindowListener(new WindowAdapter(){public void windowClosing(WindowEvent evt){ exitAccion();}});
 	}
-	
+
 	private void prepareElementos(){
 		setTitle("EnLinea");
 		setSize(500, 500);
@@ -27,7 +27,7 @@ public class EnLineaGUI extends JFrame{
 	}
 
 	private void prepareAcciones(){
-
+		setDefaultCloseOperation(0);
 	}
 
 	private void centre(){
@@ -35,5 +35,10 @@ public class EnLineaGUI extends JFrame{
 		int x = (screen.width-getSize().width)/2;
 		int y = (screen.height-getSize().height)/2;
 		setLocation(x,y);
+	}
+
+	private static  void exitAccion(){
+		if (JOptionPane.showConfirmDialog(null,"Esta seguro que desea salir? ", "Salir del sistema",JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
+			System.exit(0);
 	}
 }
